e4f6463d30262401cbf5d7bf5a15138b753f6ef3
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-05-06 10:15:12 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/rodrigo/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.rodrigo.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String message) {
+		super(message);
+	}
+}
